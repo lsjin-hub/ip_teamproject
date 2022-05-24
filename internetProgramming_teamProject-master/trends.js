0d0318894a333fba3841f35199a6d442adf016df
lsjin-hub <ascs25@naver.com>
2022-05-24 22:55:00 +0900

    trends 수정본

---
+++ internetProgramming_teamProject-master/trends.js
@@ -0,0 +1,16 @@
+$(function() {
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 250) {
+        $('#topBtn').fadeIn();
+      } else {
+        $('#topBtn').fadeOut();
+      }
+    });
+  
+    $("#topBtn").click(function() {
+      $('html, body').animate({
+        scrollTop : 0
+      }, 400);
+      return false;
+    });
+  });
